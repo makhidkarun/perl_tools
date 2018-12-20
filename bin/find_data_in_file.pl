8248c5cebf14b4035b3e9a8c7f9fa372aaef2349
Leam Hall <leamhall@gmail.com>
2018-12-20 10:17:46 -0500

    Still on find_data_in_file.pl.

--- bin/find_data_in_file.pl
+++ bin/find_data_in_file.pl
@@ -1,25 +1,35 @@
 #!/usr/bin/env perl
 #
+# find_data_in_file.pl
+#
+# Usage:
+#   find_data_in_file.pl -d "my_dir" -p "some pattern"
+#    or --dir
+#       --pattern
+
 use strict;
 use warnings;
-use Getopt::Std;
+use Getopt::Long;
 use 5.008_004;
 
 my %args;
-getopts("d:p:", \%args);
-my $start_dir = $args{d};
-my $pattern   = $args{p};
-
+my $dir;
+my $pattern;
 my @dirs = ();
 my %seen;
 
-if (! $start_dir) {
-  $start_dir = `pwd`;
-  chomp($start_dir);
-} elsif (! -d $start_dir) {
-  die "$start_dir is not a directory!";
+GetOptions(
+  "dir=s"     => \$dir,
+  "pattern=s" => \$pattern,
+);
+
+if (! $dir) {
+  $dir = `pwd`;
+  chomp($dir);
+} elsif (! -d $dir) {
+  die "$dir is not a directory!";
 }
-push @dirs, $start_dir;
+push @dirs, $dir;
 
 while (my $pwd = shift @dirs) {
   opendir(DIR, "$pwd") or die "Cannot open $pwd.\n";
