65c5e69b9b241feed6d089218b625814a42ec60b
Leam Hall <leamhall@gmail.com>
2019-03-30 08:14:22 -0400

    Moded to using Dice object.

--- bin/roll_dice.pl
+++ bin/roll_dice.pl
@@ -7,7 +7,7 @@ use warnings;
 use 5.008_004;  # Leam's personal limitation.  
 use Getopt::Long;
 use lib 'lib';
-use Dice qw( roll_dice roll_1 roll_2);
+use FTL_FleetOps::Dice qw( roll_dice roll_1 roll_2);
 
 my %args;
 my $rolls = 1;
@@ -16,7 +16,8 @@ GetOptions(
   "rolls=i" => \$rolls,
   );
 
+my $dice = FTL_FleetOps::Dice->new();
 for (1..$rolls) {
-  print roll_2 . " ";
+  print $dice->roll_2() . " ";
 }
 print "\n";
